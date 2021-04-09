efd17dce165dc6a854d754c8fc4585d035458b44
Jeremy Banka <jeremybanka@gmail.com>
2021-04-09 15:08:24 -0700

    ✅ pass phase change, view change, and Pizza integration tests

--- src/js/core.js
+++ src/js/core.js
@@ -1,3 +1,5 @@
+/* eslint func-names: 0 */
+
 function Session() {
   this.order = []
   this.idTicker = -1
@@ -22,6 +24,42 @@ function Session() {
       view: `splash-screen`,
     },
   ]
+}
+
+Session.prototype.passPhase = function () {
+  const jobDone = !this.phase.isUnfinished
+  if(jobDone) {
+    this.nextPhases.push(this.phase)
+    this.phase = this.nextPhases.shift()
+  }
+  return jobDone
+}
+
+Session.prototype.addToOrder = function (pizza) {
+  const inWrongPhase = this.phase.id !== `order-in-progress`
+  if(inWrongPhase) return false
+  // throw new Error(`It's not time to add to your order.`)
+  const thatsNoPizza = pizza?.constructor?.name !== `Pizza`
+  if(thatsNoPizza) return false
+  // throw new Error(`Sir, this is a pizza restaurant.`)
+  this.idTicker += 1
+  pizza.id = this.idTicker
+  this.order.push(pizza)
+  this.phase.otherViews.push(pizza.id)
+  return true
+}
+
+Session.prototype.changeView = function (viewId) {
+  const isCurrentView = this.phase.view === viewId
+  if(isCurrentView) return
+  const didFindOtherView = this.phase.otherViews?.includes(viewId) || false
+  if(didFindOtherView) {
+    this.phase.otherViews.unshift(this.phase.view)
+    const idxOfNewView = this.phase.otherViews.indexOf(viewId)
+    this.phase.otherViews.splice(idxOfNewView, 1)
+    this.phase.view = viewId
+  }
+  return didFindOtherView
 }
 
 function Pizza() {
