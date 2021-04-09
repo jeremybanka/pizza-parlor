63c2638d059d685ebb8798da8b7cf3889d549c7c
Jeremy Banka <jeremybanka@gmail.com>
2021-04-09 15:06:42 -0700

    ❓ add Session unit tests for view transitions within a phase

--- test/Session.test.js
+++ test/Session.test.js
@@ -68,31 +68,56 @@ test(`Session won't pass unfinished phase.`, () => {
   const sampleSession = new Session()
   const output1 = sampleSession.passPhase()
   const output2 = sampleSession.passPhase() // let's try to go two phases forward
-  const expected = {
-    order: [],
-    idTicker: -1,
-    phase: {
-      id: `order-in-progress`,
-      view: `list`,
-      otherViews: [],
-      isUnfinished: true, // oops! hold it right there, bud.
-    },
-    nextPhases: [
-      {
-        id: `order-under-review`,
-        view: `list`,
-      },
-      {
-        id: `order-complete`,
-        view: `splash-screen`,
-      },
-      {
-        id: `welcome`,
-        view: `splash-screen`,
-      },
-    ],
+  const expectedPhase =  {
+    id: `order-in-progress`,
+    view: `list`,
+    otherViews: [],
+    isUnfinished: true, // oops! hold it right there, bud.
   }
+  const expectedNextPhases =  [
+    {
+      id: `order-under-review`,
+      view: `list`,
+    },
+    {
+      id: `order-complete`,
+      view: `splash-screen`,
+    },
+    {
+      id: `welcome`,
+      view: `splash-screen`,
+    },
+  ]
   expect(output1).toBe(true)
   expect(output2).toBe(false)
-  expect(sampleSession).toEqual(expected)
+  expect(sampleSession.phase).toEqual(expectedPhase)
+  expect(sampleSession.nextPhases).toEqual(expectedNextPhases)
+})
+
+test(`Session changeView(currentView) returns undefined.`, () => {
+  const sampleSession = new Session()
+  const originalView = sampleSession.phase.view
+  const output = sampleSession.changeView(originalView)
+  expect(typeof output).toBe(`undefined`)
+  expect(sampleSession.phase.view).toEqual(originalView)
+})
+
+test(`Session changeView(nonExistentView) returns false.`, () => {
+  const sampleSession = new Session()
+  const originalView = sampleSession.phase.view
+  const nonExistentView = `this-view-does-not-exist`
+  const output = sampleSession.changeView(nonExistentView)
+  expect(output).toBe(false)
+  expect(sampleSession.phase.view).toEqual(originalView)
+})
+
+test(`Session changeView(existentView) puts currentView at otherViews[0], changes view, & returns true.`, () => {
+  const sampleSession = new Session()
+  const originalView = sampleSession.phase.view
+  const exampleView = `example-view`
+  sampleSession.phase.otherViews = [exampleView]
+  const output = sampleSession.changeView(exampleView)
+  expect(output).toBe(true)
+  expect(sampleSession.phase.view).toEqual(exampleView)
+  expect(sampleSession.phase.otherViews[0]).toEqual(originalView)
 })
