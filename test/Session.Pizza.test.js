80430aef7d2a81e36f32849cce641465d4406867
Jeremy Banka <jeremybanka@gmail.com>
2021-04-09 16:08:49 -0700

    ❓add tests for deleting order items and quick-add method

--- test/Session.Pizza.test.js
+++ test/Session.Pizza.test.js
@@ -39,3 +39,26 @@ test(`Adding a pizza during the order-in-progress phase adds your pizza and retu
   expect(output).toBe(true)
   expect(sampleOrder).toEqual([perfectPizza])
 })
+
+test(`Session.prototype.addPizza makes a pizza and adds it to the order`, () => {
+  const sampleSession = new Session()
+  const perfectPizza = new Pizza()
+  perfectPizza.id = 0 // simulate adding our new pizza to the order
+  sampleSession.passPhase()
+  const phaseId = sampleSession.phase.id
+  const addPizzaSuccess = sampleSession.addPizza()
+  const sampleOrder = sampleSession.order
+  expect(phaseId).toBe(`order-in-progress`)
+  expect(addPizzaSuccess).toBe(true)
+  expect(sampleOrder).toEqual([perfectPizza])
+})
+
+test(`Session.prototype.removeFromOrder(0) removes pizza 0 and returns true.`, () => {
+  const sampleSession = new Session()
+  sampleSession.passPhase()
+  sampleSession.addPizza()
+  const output = sampleSession.removeFromOrder(0)
+  const sampleOrder = sampleSession.order
+  expect(output).toBe(true)
+  expect(sampleOrder).toEqual([])
+})
